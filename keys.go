53a840b632fcd976054d9ed25f6ef0943caae3d5
John Doe <fpfangpin@hotmail.com>
2023-12-15 21:13:19 +0800

    fix lint hint

--- keys.go
+++ keys.go
@@ -2,7 +2,7 @@ package linq
 
 func Keys[T comparable, R any](source map[T]R) []T {
 	target := []T{}
-	for k, _ := range source {
+	for k := range source {
 		target = append(target, k)
 	}
 	return target
