75013fa59bb1a25b8f83f829eb7cdf65c086cf1a
John Doe <fpfangpin@hotmail.com>
2023-12-15 22:09:08 +0800

    go take test

--- take_test.go
+++ take_test.go
@@ -10,4 +10,7 @@ func TestTake(t *testing.T) {
 	source := []int{1, 2, 3, 4}
 	target := Take(source, 3)
 	assert.Equal(t, target, []int{1, 2, 3})
+
+	assert.Equal(t, Take(source, -1), []int{})
+	assert.Equal(t, Take(source, 6), []int{1, 2, 3, 4})
 }
